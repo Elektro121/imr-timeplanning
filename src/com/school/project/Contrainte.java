a75df4cbdbeab149eb2dba51ae7fa0cf6eda70dd
Elektro Korobase <korobase.elektro@gmail.com>
2015-02-08 12:20:08 +0100

    Création de la classe contrainte Elle demande une ressource, une plage horaire et une raison. La contrainte peut être Hebdomadaire, avec choix des jours de la semaine (Tableau de int contenant les constantes du GregorianCalendar), ou tout simplement une journée.

--- src/com/school/project/Contrainte.java
+++ src/com/school/project/Contrainte.java
@@ -1,8 +1,15 @@
 package com.school.project;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 /**
  * Created on 05/02/2015.
  */
 public class Contrainte {
-    
+    private ArrayList<PlageHoraire> plagesContraintes;
+    private ArrayList<int> repetitionJoursSemaine;
+    private GregorianCalendar Jour;
+    private Ressource ressourceContrainte;
+    private String raison;
 }
--- src/com/school/project/Ressource.java
+++ src/com/school/project/Ressource.java
@@ -4,4 +4,5 @@ package com.school.project;
  * Created on 05/02/2015.
  */
 public class Ressource {
+    private String nom;
 }
