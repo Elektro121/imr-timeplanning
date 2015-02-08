947885163d1f62e17ff1257b24477a619768d628
Elektro Korobase <korobase.elektro@gmail.com>
2015-02-08 12:10:17 +0100

    Petite erreur de ma part, les types de cours sont à mettre dans Pavé (+début du choix des variables)

--- src/com/school/project/Matiere.java
+++ src/com/school/project/Matiere.java
@@ -4,8 +4,4 @@ package com.school.project;
  * Created on 05/02/2015.
  */
 public class Matiere extends Ressource {
-    public enum Type {
-        CM, TD, TP, PROJET, DS,
-    }
-    
 }
--- src/com/school/project/Pave.java
+++ src/com/school/project/Pave.java
@@ -4,7 +4,10 @@ package com.school.project;
  * Created on 05/02/2015.
  */
 public class Pave {
-    private Type type;
+    public enum TypeCours {
+        CM, TD, TP, PROJET, DS,
+    }
+    private TypeCours type;
     private Salle salle;
     private java.util.ArrayList<Intervenant> intervenants;
     private java.util.ArrayList<GroupeEtudiants> groupes;
