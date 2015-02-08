831ad2130bfd8783d9716a13f634d32ed13575e0
Elektro Korobase <korobase.elektro@gmail.com>
2015-02-08 12:33:29 +0100

    Code des énumérateurs et complétion des variables

--- src/com/school/project/Salle.java
+++ src/com/school/project/Salle.java
@@ -1,10 +1,49 @@
 package com.school.project;
 
+import java.lang.reflect.Type;
+
 /**
  * Created on 05/02/2015.
  */
 public class Salle extends Ressource {
     public enum TypeSalle {
-        AMPHI, CM_TD, DS_TD, TP, LANGUES,
+        AMPHI("Amphithéatre"),
+        CM_TD("Salle de CM-TD"),
+        DS_TD("Salle de DS-TD"),
+        TP("Salle de TP"),
+        LANGUES("Laboratoire de langues");
+
+        private String name = "";
+
+        TypeSalle(String name) {
+            this.name = name;
+            
+        }
+        @Override
+        public String toString() {
+            return name;
+        }
     }
+    public enum Equipements {
+        TAB_CRAIE("Tableau pour craie"),
+        TAB_MARQUEUR("Tableau pour marqueur"),
+        VID_PROJ("Vidéo-Projecteur"),
+        VISIOCONF("Equipement Visio-Conférence");
+        
+        private String name = "";
+
+        TypeSalle(String name) {
+            this.name = name;
+
+        }
+        @Override
+        public String toString() {
+            return name;
+        }
+        
+    }
+    TypeSalle type;
+    boolean bComputersOnWindows;
+    int capacity;
+    
 }
