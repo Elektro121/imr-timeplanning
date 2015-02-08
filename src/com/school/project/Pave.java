2a7da9aa98b14846209d3a97f7fba83b9699756d
Elektro Korobase <korobase.elektro@gmail.com>
2015-02-08 12:05:28 +0100

    On détache Pavé de Ressources (C'était débile de le mettre ici en fait)

--- src/com/school/project/Pave.java
+++ src/com/school/project/Pave.java
@@ -3,5 +3,12 @@ package com.school.project;
 /**
  * Created on 05/02/2015.
  */
-public class Pave extends Ressource {
+public class Pave {
+    private Type type;
+    private Salle salle;
+    private java.util.ArrayList<Intervenant> intervenants;
+    private java.util.ArrayList<GroupeEtudiants> groupes;
+    private PlageHoraire plage;
+    private java.util.GregorianCalendar jour;
+    private Matiere matiere;
 }
