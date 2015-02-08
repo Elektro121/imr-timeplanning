d497482d2b497b484abe38dd18fafafed675a070
Elektro Korobase <korobase.elektro@gmail.com>
2015-02-08 12:06:14 +0100

    Constantes en majuscules

--- src/com/school/project/PlageHoraire.java
+++ src/com/school/project/PlageHoraire.java
@@ -4,5 +4,5 @@ package com.school.project;
  * Created on 05/02/2015.
  */
 public enum PlageHoraire {
-    p_8h15_10h15, p_10h30_12h30, p_14h00_16h00, p_16h15_18h15
+    P_8H15_10H15, P_10H30_12H30, P_14H00_16H00, P_16H15_18H15
 }
